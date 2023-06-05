0a18d0ec6e786cb9b9bf0c1a75ac1ef3cc13d5aa
ravi5269 <ravipatelkant@gmail.com>
2023-06-05 12:48:57 +0530

    base model renamed

--- base/models.py
+++ base/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class TimeStampModel(models.Model):
+class BaseModel(models.Model):
     created_at = models.DateTimeField(("created_at"), auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(("updated_at"), auto_now=True, auto_now_add=False)
 
