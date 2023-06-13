ed488307e4ffb422f09188f92e0d2e3434b4f0e4
ravi5269 <ravipatelkant@gmail.com>
2023-06-13 18:30:46 +0530

    removed extra line

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,6 @@ from users.views import UserAPIView
 
 urlpatterns = [
     path("register/", UserRegisterAPIView.as_view()),
-    path("register/<int:pk>", UserRegisterAPIView.as_view(), name="user"),
     path("user/", UserAPIView.as_view()),
     path("user/<int:pk>", UserAPIView.as_view())
 ]
