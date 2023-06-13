5f7d951c4cbd3c6ac9a89d4ef01af5dc13f57807
ravi5269 <ravipatelkant@gmail.com>
2023-06-13 18:47:38 +0530

    remove try catch

--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, UserRegisterSerializer
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
@@ -20,19 +20,19 @@ class UserRegisterAPIView(APIView):
         try:
             if User.objects.filter(username=requested_data.get("username")):
                 return Response(
-                    {"message": "user already exists", "status": False},
+                    {"message": "User already exists", "status": False},
                     status=status.HTTP_302_FOUND,
                 )
-            serializer = UserSerializer(data=requested_data)
+            serializer = UserRegisterSerializer(data=requested_data)
             if serializer.is_valid(raise_exception=True):
                 serializer.create(validated_data=requested_data)
                 return Response(
-                    {"message": " user registrations successful", "status": True},
+                    {"message": " User registrations successful", "status": True},
                     status=status.HTTP_201_CREATED,
                 )
         except Exception as e:
             return Response(
-                {"message": "user already register", "status": False},
+                {"message": "User already register", "status": False},
                 status=status.HTTP_302_FOUND,
             )
 
@@ -41,42 +41,29 @@ class UserAPIView(APIView):
     permission_classes = (IsAuthenticated,)
 
     def delete(self, request, pk=None):
-        try:
-            user = User.objects.get(pk=pk)
-            user.delete()
-            return Response(
-                {"message": "success", "status": True},
-                status=status.HTTP_204_NO_CONTENT,
-            )
-        except Exception as e:
-            return Response(
-                {"message": "user not found ", "status": False},
-                status=status.HTTP_302_FOUND,
-            )
+        user = get_object_or_404(User, pk=pk)
+        user.delete()
+        return Response(
+            {"message": "Success", "status": True},
+            status=status.HTTP_204_NO_CONTENT,
+        )
 
     def get(self, request, pk=None):
-        try:
-            if pk:
-                user = User.objects.get(id=pk)
-                serializer = UserSerializer(user)
+        if pk:
+            user = get_object_or_404(User, pk=pk)
+            serializer = UserSerializer(user)
 
-                return Response(
-                    {"message": "success", "data": serializer.data},
-                    status=status.HTTP_200_OK,
-                )
-
-            users = User.objects.all()
-            serializer = UserSerializer(users, many=True)
-            print(request.user)
             return Response(
-                {"message": "success", "data": serializer.data},
+                {"message": "Success", "data": serializer.data},
                 status=status.HTTP_200_OK,
             )
-        except Exception as e:
-            return Response(
-                {"message": "user not found ", "status": False},
-                status=status.HTTP_302_FOUND,
-            )
+
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(
+            {"message": "Success", "data": serializer.data},
+            status=status.HTTP_200_OK,
+        )
 
     def put(self, request, pk=None):
         user = get_object_or_404(User, pk=pk)
